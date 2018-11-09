a904e2399fc8da155e4a1b56fe36d66a77d7ef18
Stephane Tinseau <stephane.tinseau@thomsonreuters.com>
2018-11-09 10:39:24 +0100

    Add provider routes

---
+++ routes/provider.route.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const config = require('../modules/common/config')
+const passport = require('../modules/common/passport')
+const jwt = require('jsonwebtoken')
+const Provider = require('../models/provider')
+
+/* GET /provider */
+router.get('/', passport.authenticate('jwt', { session: false }) , (req, res) => { 
+
+})
+
+/* GET /provider/:providerid */
+router.get('/:userid', passport.authenticate('jwt', { session: false }) , (req, res) => { 
+
+})
+
+/* POST /provider */
+router.post('/', passport.authenticate('jwt', { session: false }) , (req, res) => { 
+
+})
+
+/* PUT /provider/:providerid */
+router.put('/:providerid', passport.authenticate('jwt', { session: false }) , (req, res) => { 
+
+})
+
+/* DELETE /provider/:providerid */
+router.delete('/:providerid', passport.authenticate('jwt', { session: false }) , (req, res) => { 
+
+})
+
+/* POST /provider/:providerid/search */
+router.post('/:providerid/search', (req, res) => { 
+
+})
+
+module.exports = router
